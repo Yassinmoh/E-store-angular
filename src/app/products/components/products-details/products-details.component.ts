887958f550530632b076d9c74fe3edf728bcc96c
Yassinmoh <yassin_mohamed310@outlook.com>
2022-12-26 11:47:27 +0200

    create navbar component

--- src/app/products/components/products-details/products-details.component.ts
+++ src/app/products/components/products-details/products-details.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products-details',
   templateUrl: './products-details.component.html',
-  styleUrls: ['./products-details.component.css']
+  styleUrls: ['./products-details.component.scss']
 })
 export class ProductsDetailsComponent {
 
